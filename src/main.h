2932b9ef8c92cf1dc84818cfc373403561e24a47
cryptocoindude <bechtel.bp@comcast.net>
2014-05-09 10:58:27 -0400

    More Changes
    Changes

--- src/main.h
+++ src/main.h
@@ -36,8 +36,8 @@ static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const unsigned int MAX_INV_SZ = 30000;
 static const int64 MIN_TX_FEE = .001 * COIN;
 static const int64 MIN_RELAY_TX_FEE = .001 * COIN;
-static const int64 MAX_MONEY = 10000000 * COIN;
-static const int64 MAX_MONEY2 = 10000000 * COIN;			// 10 mil
+static const int64 MAX_MONEY = 50000000 * COIN;
+static const int64 MAX_MONEY2 = 50000000 * COIN;			// 10 mil
 static const int64 CIRCULATION_MONEY = MAX_MONEY2;
 static const int64 MAX_MINT_PROOF_OF_STAKE = 0.2333 * COIN;	// 0.2333% annual interest
 
